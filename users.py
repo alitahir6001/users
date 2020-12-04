4494195aa47ca636ff5b2be643539daf03f8713d
Ali Tahir <alitahir6001@gmail.com>
2020-12-04 14:21:16 -0500

    small note edits

--- users.py
+++ users.py
@@ -1,3 +1,4 @@
+
 class User:
     def __init__(self, name, email_address):# now our method has 2 parameters/aka arguments!
         self.name = name			# and we use the values passed in to set the name attribute
@@ -32,6 +33,6 @@ print(guido.account_balance)
 
 guido.make_deposit(5000).make_deposit(100).make_deposit(50).make_withdrawl(500).display_user_balance()
 monty.make_deposit(1000000).make_deposit(0.50).make_withdrawl(500000).make_withdrawl(1).display_user_balance()
-ali.make_deposit(500000).make_withdrawl(500).make_withdrawl(100).make_withdrawl(353.45).display_user_balance()
+ali.make_deposit(500000).make_withdrawl(500).make_withdrawl(100).make_withdrawl(353.45).display_user_balance()  # this is the chaining method mentioned in the assignment. When I was getting help from my cohort-mates about this assignment, they suggested adding a return statement to my methods to "help make my life easier" an lo and behold, this is exactly what the next assignment was.
 
 # I spent WAY too much time on this one assignment but after I asked for help I realized I was repeating attributes inside the functions that the console wasn't recognizing.
